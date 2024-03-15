fa1abcac75dc56ef0b99d3e3db5f2c4db8031392
Ryan Sather <ryan@ryansather.com>
2024-03-15 17:35:20 -0500

    Water flows over Minnow Trap

--- src/main/java/net/cookiebrain/youneedbait/block/custom/MinnowTrapBlock.java
+++ src/main/java/net/cookiebrain/youneedbait/block/custom/MinnowTrapBlock.java
@@ -4,19 +4,49 @@ import com.mojang.serialization.MapCodec;
 import net.cookiebrain.youneedbait.block.entity.MinnowTrapBlockEntity;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 
-public class MinnowTrapBlock extends BlockWithEntity implements BlockEntityProvider {
+public class MinnowTrapBlock extends BlockWithEntity implements BlockEntityProvider, Waterloggable {
     private static final VoxelShape MINNOW_TRAP_SHAPE = MinnowTrapBlock.createCuboidShape(3, 1, 0, 14, 11, 15);
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+    //public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
+    //private final Block baseBlock;
+    //protected final BlockState baseBlockState;
     public MinnowTrapBlock(Settings settings) {
         super(settings);
+        setDefaultState(stateManager.getDefaultState().with(WATERLOGGED, false));
     }
-
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        super.appendProperties(builder);
+        builder.add(WATERLOGGED);
+    }
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        BlockPos blockPos = ctx.getBlockPos();
+        FluidState fluidState = ctx.getWorld().getFluidState(blockPos);
+        return this.getDefaultState().with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
+    }
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        if (state.get(WATERLOGGED).booleanValue()) {
+            return Fluids.WATER.getStill(false);
+        }
+        return super.getFluidState(state);
+    }
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         return MINNOW_TRAP_SHAPE;
@@ -39,15 +69,24 @@ public class MinnowTrapBlock extends BlockWithEntity implements BlockEntityProvi
         return new MinnowTrapBlockEntity(pos,state);
     }
 
-    @Override
-    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
-        if (state.getBlock() != newState.getBlock()) {
-            BlockEntity blockEntity = world.getBlockEntity(pos);
-            if (blockEntity instanceof MinnowTrapBlockEntity) {
-                ItemScatterer.spawn(world, pos, (MinnowTrapBlockEntity)blockEntity);
-                world.updateComparators(pos,this);
-            }
-            super.onStateReplaced(state, world, pos, newState, moved);
-        }
-    }
+
+//    @Override
+//    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+//        if (state.get(WATERLOGGED).booleanValue()) {
+//            world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+//        }
+//        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+//    }
+//
+//    @Override
+//    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+//        if (state.getBlock() != newState.getBlock()) {
+//            BlockEntity blockEntity = world.getBlockEntity(pos);
+//            if (blockEntity instanceof MinnowTrapBlockEntity) {
+//                ItemScatterer.spawn(world, pos, (MinnowTrapBlockEntity)blockEntity);
+//                world.updateComparators(pos,this);
+//            }
+//            super.onStateReplaced(state, world, pos, newState, moved);
+//        }
+//    }
 }
